ec629368c14f49593f98d4e5611c9ae37115598f
Sam Gilbert <sam.gilbert@ucl.ac.uk>
2023-05-23 12:09:41 +0100

    initial working version

--- WebTasks1/src/com/sam/webtasks/patternCopy/PatternDisplay.java
+++ WebTasks1/src/com/sam/webtasks/patternCopy/PatternDisplay.java
@@ -3,12 +3,20 @@ package com.sam.webtasks.patternCopy;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.ait.lienzo.client.core.event.NodeDragEndEvent;
+import com.ait.lienzo.client.core.event.NodeDragEndHandler;
+import com.ait.lienzo.client.core.event.NodeMouseClickEvent;
+import com.ait.lienzo.client.core.event.NodeMouseClickHandler;
+import com.ait.lienzo.client.core.shape.Group;
 import com.ait.lienzo.client.core.shape.Layer;
 import com.ait.lienzo.client.core.shape.Line;
 import com.ait.lienzo.client.core.shape.Rectangle;
+import com.ait.lienzo.client.core.shape.Text;
 import com.ait.lienzo.client.widget.LienzoPanel;
 import com.ait.lienzo.shared.core.types.Color;
 import com.ait.lienzo.shared.core.types.ColorName;
+import com.ait.lienzo.shared.core.types.TextAlign;
+import com.ait.lienzo.shared.core.types.TextBaseLine;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.HasVerticalAlignment;
@@ -20,9 +28,11 @@ public class PatternDisplay {
 		
 	/*------------display parameters------------*/	
 	public static int gridSize = 5; 	  //how many squares?
-	public static int gridPixels = 40;    //size of each square in pixels
-	public static int gridLineWidth = 4;
-	public static int nFilledSquares = 5; //how many filled squares?
+	public static int gridPixels = 60;    //size of each square in pixels
+	public static int gridLineWidth = 5;
+	public static int nFilledSquares = 10; //how many filled squares?
+	public static int copyRows = 2;        //how many rows in the initial display of the copy squares?
+	public static int copyCols = 5;        //how many columjns in the initial display of the copy squares?
 	public static final int panelSize = (3 * gridPixels * gridSize) + (2 * gridLineWidth);
 	
 	/*------------initialise Lienzo objects------------*/
@@ -41,12 +51,23 @@ public class PatternDisplay {
 	public static Line[] rightHorizontalLines = new Line[gridSize+1];
 	public static Line[] rightVerticalLines = new Line[gridSize+1];
 	
+	//we have one rectangle for each possible square in the template grid
 	public static final Rectangle[] templateRectangles = new Rectangle[gridSize * gridSize];
-    public static final Rectangle[] copyRectangles = new Rectangle[gridSize * gridSize];
 	
-    public static List<Integer> filledSquares = new ArrayList<Integer>();
+	//but for the copy squares, we only need one for each filled square, which can be dragged into position
+    public static final Rectangle[] copyRectangles = new Rectangle[nFilledSquares];
     
     public static Color[] palette = new Color[nFilledSquares];  
+    
+    //list of all possible squares, which we use to generate the pattern, initialise copy square, and check response
+    public static List<Integer> allSquares = new ArrayList<Integer>();
+    public static List<Integer> allCopySquares = new ArrayList<Integer>();
+    
+    //use this variable to check whether all copy squares have been placed
+    public static Boolean[] copySquarePlaced = new Boolean[nFilledSquares];
+    public static Boolean allSquaresPlaced = false;
+    
+    public static final Text finishText = new Text("Finished", "Verdana, sans-serif", null, 20);
 
     /*------------set up Lienzo objects------------*/
 	public static void Init() {
@@ -61,12 +82,12 @@ public class PatternDisplay {
         wrapper.add(lienzoWrapper);
 		
 		//gridlines
+        final int rightOffset = 2*gridPixels*gridSize;
+        
         for (int i = 0; i <= gridSize; i++) {
             leftHorizontalLines[i] = new Line(gridLineWidth/2,i*gridPixels+gridLineWidth,gridPixels*gridSize+(1.5*gridLineWidth),i*gridPixels+gridLineWidth).setStrokeColor(ColorName.BLACK).setStrokeWidth(gridLineWidth);
             leftVerticalLines[i] = new Line(i*gridPixels+gridLineWidth,gridLineWidth,i*gridPixels+gridLineWidth,gridPixels*gridSize+gridLineWidth).setStrokeColor(ColorName.BLACK).setStrokeWidth(gridLineWidth);
 
-            int rightOffset = 2*gridPixels*gridSize;
-            
             rightHorizontalLines[i] = new Line(gridLineWidth/2+rightOffset,i*gridPixels+gridLineWidth,gridPixels*gridSize+(1.5*gridLineWidth)+rightOffset,i*gridPixels+gridLineWidth).setStrokeColor(ColorName.BLACK).setStrokeWidth(gridLineWidth);
             rightVerticalLines[i] = new Line(i*gridPixels+gridLineWidth+rightOffset,gridLineWidth,i*gridPixels+gridLineWidth+rightOffset,gridPixels*gridSize+gridLineWidth).setStrokeColor(ColorName.BLACK).setStrokeWidth(gridLineWidth);
             
@@ -81,9 +102,18 @@ public class PatternDisplay {
         palette[1] = new Color(255,127,0); 
         palette[2] = new Color(255,0,0); 
         palette[3] = new Color(0,255,255); 
-        palette[4] = new Color(0,238,0); 
+        palette[4] = new Color(0,238,0);
+        palette[5] = new Color(0,100,0);
+        palette[6] = new Color(255,255,0);
+        palette[7] = new Color(255,20,147);
+        palette[8] = new Color(255,228,196);
+        palette[9] = new Color(139,71,38);
+        palette[10]= new Color(160,32,240);
+        palette[11]= new Color(105,105,105);
 
         /*----------set up stimulus squares------------*/
+        
+        //one template square for each grid position
         int rectangleIndex = 0;
 
         for (int x = 0; x < gridSize; x++) {
@@ -97,19 +127,115 @@ public class PatternDisplay {
                 templateLayer.add(templateRectangles[rectangleIndex++]);
             }
         }
+        
+        //add the 'finished' button
+        final Group finishGroup = new Group();
+      
+        finishText.setFillColor(ColorName.BLACK).setAlpha(0.5).setTextAlign(TextAlign.CENTER).setTextBaseLine(TextBaseLine.MIDDLE);
+        
+        int rectX=130,rectY=50;
+        
+        final Rectangle finishRectangle = new Rectangle(rectX,rectY);
+        finishRectangle.setFillColor(ColorName.GREENYELLOW).setFillAlpha(0.3).setCornerRadius(10);
+        finishGroup.add(finishText);
+        finishGroup.add(finishRectangle);
+        finishText.setX(rectX/2).setY(rectY/2);
+        
+        gridLayer.add(finishGroup);
+        finishGroup.setX(1.5*gridPixels*gridSize-rectX/2);
+        finishGroup.setY(0.666*gridPixels*gridSize);
+        
+        
+        finishRectangle.addNodeMouseClickHandler(new NodeMouseClickHandler() {
+        	public void onNodeMouseClick (NodeMouseClickEvent event) {
+        		if (allSquaresPlaced) {
+        			//check if correct
+        			Boolean allCorrect=true;
+        			
+        			for (int i=0; i<nFilledSquares; i++) {
+        				//Window.alert("allsquare: " + allSquares.get(i) + ", copysquare: " + allCopySquares.get(i));
+        				if (allCopySquares.get(i) != allSquares.get(i)) {
+        					allCorrect=false;	
+              			} 
+        			}
+        			
+        			if (allCorrect) {
+        				Window.alert("Correct");
+        			} else {
+        				Window.alert("Incorrect");
+        			}
+        		}
+        	}
+        });
+        
+        //add the display switch button
 
-        rectangleIndex = 0;
+        //now set up the copy rectangles. their colours and locations are set up in the PatternTrial code
+        for (int i = 0; i < nFilledSquares; i++) {
+        	final int finali = i; //use this to reference which square was clicked
+        	
+        	copyRectangles[i] = new Rectangle(gridPixels - (2*gridLineWidth), gridPixels - (2*gridLineWidth));
+        	copyRectangles[i].setDraggable(true).setFillColor(palette[i]);
+        	copyLayer.add(copyRectangles[i]);
+        	
+        	//make the copy rectangles snap to the grid
+        	copyRectangles[i].addNodeDragEndHandler((NodeDragEndHandler) new NodeDragEndHandler() {
+				public void onNodeDragEnd(NodeDragEndEvent event) {
+					int clickedX=(int) copyRectangles[finali].getX();
+					int clickedY=(int) copyRectangles[finali].getY();
+					
+					int rectangle=0;
+					int closestDistance = gridPixels*gridPixels*gridSize*gridSize;
+					
+					//find out which grid position is closer to this x and y co-ordinate
+					for (int x = 0; x < gridSize; x++) {
+			            for (int y = 0; y < gridSize; y++) {
+			                int xPixel = x * gridPixels + 2*gridLineWidth + rightOffset;
+			                int yPixel = y * gridPixels + 2*gridLineWidth;
+			                
+			                int distance = (clickedX-xPixel)*(clickedX-xPixel) + (clickedY-yPixel)*(clickedY-yPixel);
+			                
+			                if (distance<closestDistance) {
+			                	allCopySquares.set(finali, rectangle);
+			                	closestDistance=distance;
+			                	
+			                	copyRectangles[finali].setX(xPixel).setY(yPixel);
+			                	copySquarePlaced[finali]=true;
 
-        for (int x = 0; x < gridSize; x++) {
-            for (int y = 0; y < gridSize; y++) {
-                int xPixel = x * gridPixels + 1 + (2 * gridPixels * gridSize);
-                int yPixel = y * gridPixels + 1;
-
-                copyRectangles[rectangleIndex] = new Rectangle(gridPixels - (2*gridLineWidth), gridPixels - (2*gridLineWidth));
-
-                copyRectangles[rectangleIndex].setX(xPixel).setY(yPixel);
-                copyLayer.add(copyRectangles[rectangleIndex++]);
-            }
+			                	copyLayer.draw();
+			                	
+			                	//check to see whether all squares have been placed
+			                	allSquaresPlaced=true;
+			                	
+			                	for (int i=0; i<nFilledSquares; i++) {
+			                		if (!copySquarePlaced[i]) {
+			                			allSquaresPlaced=false;
+			                		}
+			                	}
+			                	
+			                	if(allSquaresPlaced) {
+			                		finishText.setAlpha(1);
+			                		gridLayer.draw();
+			                	}
+			                }
+			                
+			                rectangle++;
+			            }
+					}
+        		}
+        	});
         }
+
+        //set up a list of all the possible squares. we randomize this to select the template pattern
+        for (int i=0; i<(gridSize*gridSize); i++) {
+        	allSquares.add(i);
+        }
+        
+        //also a list of all the copy squares. we use this to initialise them into random positions at beginning of trial, and verify response
+        for (int i=0; i<nFilledSquares; i++) {
+        	allCopySquares.add(i);
+        }
+        
+        
 	}
 }
--- WebTasks1/src/com/sam/webtasks/patternCopy/PatternTrial.java
+++ WebTasks1/src/com/sam/webtasks/patternCopy/PatternTrial.java
@@ -1,12 +1,55 @@
 package com.sam.webtasks.patternCopy;
 
-import com.sam.webtasks.perceptualTask.PerceptDisplay;
+import java.util.Collections;
+
+import com.ait.lienzo.shared.core.types.ColorName;
+import com.google.gwt.user.client.Random;
 
 public class PatternTrial {
 	public static void Run() {
 		PatternDisplay.panel.add(PatternDisplay.gridLayer);
 		PatternDisplay.panel.add(PatternDisplay.templateLayer);
-		PatternDisplay.templateRectangles[0].setFillColor(PatternDisplay.palette[0]);
+		PatternDisplay.panel.add(PatternDisplay.copyLayer);
+		
+		//randomise the full set of squares, to pick a pattern
+		for (int i = 0; i < PatternDisplay.allSquares.size(); i++) {
+			Collections.swap(PatternDisplay.allSquares, i, Random.nextInt(PatternDisplay.allSquares.size()));
+			
+			//initialise this square to white initially
+			PatternDisplay.templateRectangles[i].setFillColor(ColorName.WHITE);
+		}
+		
+		//now fill the first N squares
+		for (int i = 0; i < PatternDisplay.nFilledSquares; i++) {
+			PatternDisplay.templateRectangles[PatternDisplay.allSquares.get(i)].setFillColor(PatternDisplay.palette[i]);
+		}
+		
+		//now initialise the locations for the copy squares
+		PatternDisplay.allSquaresPlaced = false;
+		PatternDisplay.finishText.setAlpha(0.3);
+		
+		//fist randomise the list of all copy squares
+		for (int i = 0; i < PatternDisplay.nFilledSquares; i++) {
+			Collections.swap(PatternDisplay.allCopySquares,  i,  Random.nextInt(PatternDisplay.allCopySquares.size()));
+		}
+		
+		int rectangleIndex = 0;
+
+        for (int x = 0; x < PatternDisplay.copyCols; x++) {
+            for (int y = 0; y < PatternDisplay.copyRows; y++) {
+            	PatternDisplay.copySquarePlaced[rectangleIndex]=false;
+            	
+                int xPixel = x * PatternDisplay.gridPixels + 1 + (2 * PatternDisplay.gridPixels * PatternDisplay.gridSize) + 2*PatternDisplay.gridLineWidth;
+                int yPixel = y * PatternDisplay.gridPixels + 1 + 2*PatternDisplay.gridLineWidth;
+                
+                //place the copy rectangles underneath the grid
+                yPixel += 1.1 * PatternDisplay.gridPixels * PatternDisplay.gridSize;
+
+                PatternDisplay.copyRectangles[PatternDisplay.allCopySquares.get(rectangleIndex++)].setX(xPixel).setY(yPixel);
+                
+            }
+        }
+		
     	PatternDisplay.panel.draw();
 	}
 
